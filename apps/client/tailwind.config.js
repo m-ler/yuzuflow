f06fab8c64396a8d10980053323913173024bdae
miguelangellerma <miguellangel770@gmail.com>
2023-09-17 19:01:33 -0500

    Updated tailwind config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -4,7 +4,7 @@
 import { nextui } from '@nextui-org/react';
 
 module.exports = {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', `./../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}`],
 	darkMode: 'class',
 	theme: {
 		fontFamily: {
