02c9d5f3cb79a2c6d15531ba75b1f446624fca6e
miguelangellerma <miguellangel770@gmail.com>
2023-09-17 21:32:24 -0500

    Added shared package

---
+++ packages/shared/types.ts
@@ -0,0 +1,14 @@
+export type YuzuVersion = {
+	name: string
+	date: string
+	assetUrl: string
+}
+
+export type Paginated<TData> = {
+	data: TData[]
+	page: number
+	pageCount: number
+	pageSize: number
+}
+
+export type VersionsRequest = Paginated<YuzuVersion>
